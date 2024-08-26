5857a67645987a5b58ad48bfe5db1f4f7b8197d0
Gustavo Gutiérrez <a01747869@tec.mx>
2024-08-25 19:43:53 -0600

    Corrección de normalización y implemetación de esta

--- ModeloAprendizaje.py
+++ ModeloAprendizaje.py
@@ -32,16 +32,17 @@ def GradientDescent(params,samples,learn_rate,valor_y):
         avance[j] = params[j] - learn_rate*(1/len(samples)) * sum_error
     return avance
 
-def Z_score(samples):
-    muestras = np.array(samples)
-    medias = muestras.mean(axis=0)
-    desv_est = muestras.std(axis=0)
+def Normalizacion(samples):
+    samples = np.array(samples, dtype=float)
+    min_vals = np.min(samples, axis=0)
+    max_vals = np.max(samples, axis=0)
+    range_vals = max_vals - min_vals
 
-    desv_est[desv_est == 0] = 1
+    range_vals = np.clip(range_vals, a_min=1e-8, a_max=None)
 
-    muestras_estandarizadas = (muestras-medias) / desv_est
+    normalized_samples = (samples - min_vals) / range_vals
 
-    return muestras_estandarizadas.tolist()
+    return normalized_samples.tolist()
 
 def linear_regression(params,samples,valor_y,num_epochs,learning_rate):
     for i in range(len(samples)):
@@ -51,7 +52,7 @@ def linear_regression(params,samples,valor_y,num_epochs,learning_rate):
             samples[i] = [1,samples[i]]
     
     print("Antes de normalizar: " + str(samples))
-    samples = Z_score(samples)
+    samples = Normalizacion(samples)
     print("Despues de normalizar: " + str(samples))
 
     epochs = 0
@@ -68,21 +69,29 @@ def linear_regression(params,samples,valor_y,num_epochs,learning_rate):
             print("Muestras: "  + str(samples))
             print("Parametros finales: " + str(params))
             break
+    return params
 
 if __name__ == "__main__":
     #Conjunto de características de una casa (Superficie en metros cuadrados, número de recámaras)
     caracteristicas_casa = [[1400, 3], [2400, 7], [1800, 4], [1900, 5], [1300, 2], [1100, 2]]
     precio = [245000, 450000, 320000, 350000, 230000, 200000]
-    learning_rate = 0.01
+    learning_rate = 0.001
 
     #Coeficiente para las características de la casa y el término independiente
-    params = (0,0,0)
+    params = [0,0,0]
 
-    linear_regression(params,caracteristicas_casa,precio,5000,learning_rate)
+    params_finales = linear_regression(params,caracteristicas_casa,precio,5000,learning_rate)
 
     import matplotlib.pyplot as plt
     plt.plot(errores)
-    plt.show()
+    plt.xlabel('Epochs')
+    plt.ylabel('Error Medio')
+    plt.show() 
+
+    nueva_casa = [1,1500,3]
+    nueva_casa_normalizada = Normalizacion(nueva_casa)
+    prediccion_precio = hipotesis(params_finales,nueva_casa_normalizada)
+    print("El precio predicho para la nueva casa es: " + str(prediccion_precio))
 
 
 
