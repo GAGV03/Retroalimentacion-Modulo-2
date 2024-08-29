8ca53b937b9195fbb042fdd3542e899a82a7752d
Gustavo Gutiérrez <132943080+GAGV03@users.noreply.github.com>
2024-08-28 21:29:08 -0600

    Cambio de Lineal a Logistica

--- ModeloAprendizaje.py
+++ ModeloAprendizaje.py
@@ -1,37 +1,41 @@
-#Técnica de aprendizaje maquina sin framework. Gustavo Alejandro Gutiérrez Valdes A01747869
-
 import numpy as np
 
 errores = []
 
+# Función sigmoide
+def sigmoide(z):
+    return 1 / (1 + np.exp(-z))
+
+# Hipótesis de regresión logística
 def hipotesis(params, samples):
     valor_acumulado = 0
     for i in range(len(params)):
         valor_acumulado += params[i] * samples[i]
-    return valor_acumulado
+    return sigmoide(valor_acumulado)
 
-#Función cost/loss que calcula y muestra los errores obtenido por una iteración de la hipotesis
-def MSE(params, samples, valor_y):
+# Función de costo: Entropía cruzada
+def cross_entropy_loss(params, samples, valor_y):
     sum_error = 0
     for i in range(len(samples)):
-        h = hipotesis(params,samples[i])
-        error = h - valor_y[i]
-        sum_error += error ** 2
-    error_medio = sum_error/len(samples)
-    print("Error medio: " + str(error_medio))
+        h = hipotesis(params, samples[i])
+        error = -valor_y[i] * np.log(h) - (1 - valor_y[i]) * np.log(1 - h)
+        sum_error += error
+    error_medio = sum_error / len(samples)
+    print("Error medio (entropía cruzada): " + str(error_medio))
     errores.append(error_medio)
 
-def GradientDescent(params,samples,learn_rate,valor_y):
+# Descenso de gradiente para la regresión logística
+def GradientDescent(params, samples, learn_rate, valor_y):
     avance = list(params)
-    error = 0
-    for j in range (len(params)):
+    for j in range(len(params)):
         sum_error = 0
-        for i in range (len(samples)):
-            error = hipotesis(params,samples[i]) - valor_y[i]
-            sum_error = sum_error + error * samples[i][j]
-        avance[j] = params[j] - learn_rate*(1/len(samples)) * sum_error
+        for i in range(len(samples)):
+            error = hipotesis(params, samples[i]) - valor_y[i]
+            sum_error += error * samples[i][j]
+        avance[j] = params[j] - learn_rate * (1 / len(samples)) * sum_error
     return avance
 
+# Normalización de los datos
 def Normalizacion(samples):
     samples = np.array(samples, dtype=float)
     min_vals = np.min(samples, axis=0)
@@ -44,13 +48,14 @@ def Normalizacion(samples):
 
     return normalized_samples.tolist()
 
-def linear_regression(params,samples,valor_y,num_epochs,learning_rate):
+# Regresión logística
+def logistic_regression(params, samples, valor_y, num_epochs, learning_rate):
     for i in range(len(samples)):
-        if isinstance(samples[i],list):
+        if isinstance(samples[i], list):
             samples[i] = [1] + samples[i]
         else:
-            samples[i] = [1,samples[i]]
-    
+            samples[i] = [1, samples[i]]
+
     print("Antes de normalizar: " + str(samples))
     samples = Normalizacion(samples)
     print("Despues de normalizar: " + str(samples))
@@ -60,38 +65,39 @@ def linear_regression(params,samples,valor_y,num_epochs,learning_rate):
         oldparams = list(params)
         print(f"Epoch #: {epochs}")
         print("Parametros actuales: " + str(params))
-        params = GradientDescent(params,samples,learning_rate,valor_y)
-        MSE(params,samples,valor_y)
+        params = GradientDescent(params, samples, learning_rate, valor_y)
+        cross_entropy_loss(params, samples, valor_y)
         print(params)
         epochs += 1
-        if(oldparams == params or epochs == num_epochs):
+        if oldparams == params or epochs == num_epochs:
             print("ENTRENAMIENTO FINALIZADO")
-            print("Muestras: "  + str(samples))
+            print("Muestras: " + str(samples))
             print("Parametros finales: " + str(params))
             break
     return params
 
 if __name__ == "__main__":
-    #Conjunto de características de una casa (Superficie en metros cuadrados, número de recámaras)
+    # Conjunto de características de una casa (Superficie en metros cuadrados, número de recámaras)
     caracteristicas_casa = [[1400, 3], [2400, 7], [1800, 4], [1900, 5], [1300, 2], [1100, 2]]
-    precio = [245000, 450000, 320000, 350000, 230000, 200000]
+    precio = [1, 1, 0, 1, 0, 0]  # Precio transformado en 1 o 0 (binario)
     learning_rate = 0.001
 
-    #Coeficiente para las características de la casa y el término independiente
-    params = [0,0,0]
+    # Coeficiente para las características de la casa y el término independiente
+    params = [0, 0, 0]
 
-    params_finales = linear_regression(params,caracteristicas_casa,precio,5000,learning_rate)
+    params_finales = logistic_regression(params, caracteristicas_casa, precio, 5000, learning_rate)
 
     import matplotlib.pyplot as plt
     plt.plot(errores)
     plt.xlabel('Epochs')
-    plt.ylabel('Error Medio')
-    plt.show() 
+    plt.ylabel('Error Medio (Entropía Cruzada)')
+    plt.show()
 
-    nueva_casa = [1,1500,3]
+    nueva_casa = [1, 1500, 3]
     nueva_casa_normalizada = Normalizacion(nueva_casa)
-    prediccion_precio = hipotesis(params_finales,nueva_casa_normalizada)
-    print("El precio predicho para la nueva casa es: " + str(prediccion_precio))
+    probabilidad = hipotesis(params_finales, nueva_casa_normalizada)
+    print("La probabilidad predicha de que la nueva casa pertenezca a la clase 1 es: " + str(probabilidad))
+
 
 
 
